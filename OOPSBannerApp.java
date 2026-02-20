23da711f4bbecdc3d6a885bb32f131a65339362e
kv2877@srmist.edu.in <kv2877@srmist.edu.in>
2026-02-20 21:32:54 +0530

    UC3: Remove unnecessary comments

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,4 @@
-/**
- * OOPSBannerApp UC3 - OOPS Banner Application (Use Case 3)
- *
- * This class extends the functionality of Use Case 2 by improving the way the "OOPS" banner
- * is constructed and displayed. It focuses on enhancing code readability and efficiency
- * by utilizing the String.join() method to create each line of the banner. This approach
- * overcomes the drawbacks of using the + operator for string concatenation, which can lead
- * to code inefficiency due to the creation of multiple intermediate String objects in memory.
- *
- * @author Developer
- * @version 3.0
- */
+
  
  
 
