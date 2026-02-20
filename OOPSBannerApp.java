e5c9e26ecc691ede8ef90e4699aa9e11e816b72a
kv2877@srmist.edu.in <kv2877@srmist.edu.in>
2026-02-20 21:34:29 +0530

    UC5: Remove unnecessary comments

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,4 @@
-/**
- * OOPSBannerApp UC5 - Render OOPS as Banner using Inline Array Initialization
- *
- * This use case extends UC4 by defining and populating the String array in a more
- * concise way at the time of declaration using String.join() method to create each
- * line of the banner. This further enhances code readability and maintainability.
- *
- * @author Developer
- * @version 5.0
- */
-
-
-
+ 
 public class OOPSBannerApp {
 
     
