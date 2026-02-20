c1835a75d28cade6530cb454825d284fbb485ab2
kv2877@srmist.edu.in <kv2877@srmist.edu.in>
2026-02-20 21:22:16 +0530

    UC5: Remove unnecessary comments

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -9,19 +9,14 @@
  * @version 5.0
  */
 
-// Extend the user story 4 to further develop the OOPS Banner Application by
-// not only using String arrays to hold banner lines and printing them in a
-// loop. But also by defining and populating the array in a more concise way
-// at the time of declaration using String.join() method to create each line
-// of the banner.
+
 
 public class OOPSBannerApp {
 
-    // Main method to run the banner display
+    
     public static void main(String[] args) {
 
-        // Define String Array variable to hold the OOPS banner lines
-        // Each line represents a row in the banner for the letters O, O, P, S
+        
         String[] lines = {
             String.join("", "  ***  ", "  ", "  ***  ", "  ", "****** ", "  ", " *****"),
             String.join("", " ** ** ", "  ", " ** ** ", "  ", "**  ** ", "  ", "**    "),
@@ -32,8 +27,6 @@ public class OOPSBannerApp {
             String.join("", "  ***  ", "  ", "  ***  ", "  ", "**     ", "  ", " *****")
         };
 
-        // Use a loop to print each line of the banner to create the
-        // visual effect for the message "OOPS"
         for (String line : lines) {
             System.out.println(line);
         }
